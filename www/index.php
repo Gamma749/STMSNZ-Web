b91ba85588f2e176f15759707614bee70c777e6c
Hayden McAlister <haydenmcalister49@gmail.com>
2021-10-30 23:05:51 +1300

    Add the start of the site

---
+++ www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>STMSNZ- Home</title>
+    <link rel='shortcut icon' type='image/x-icon' href='images/favicon.ico' />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="nav-style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet" />
+
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="STMSNZ, STMS NZ, STMS, Traffic Managment">
+    <meta name="author" content="Hayden McAlister">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+    <?php include "navbar.php"?>
+
+</body>
