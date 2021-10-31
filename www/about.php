15111110e9bc42e2b3b1afbf41a4a3fcf0407e07
Hayden McAlister <haydenmcalister49@gmail.com>
2021-11-01 01:42:16 +1300

    Add base code for all other pages in site

---
+++ www/about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>STMSNZ- About</title>
+    <link rel='shortcut icon' type='image/x-icon' href='images/favicon.ico' />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="nav-style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet" />
+
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="STMSNZ, STMS NZ, STMS, Traffic Managment">
+    <meta name="author" content="Hayden McAlister">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+    <?php include "navbar.php" ?>
+
+    <main>
+        <?php
+        $pagename = "About";
+        include "header.php"
+        ?>
+        <h2>Our Story</h2>
+        <p>STMSNZ owners Paul and Hazel Guy started their own Traffic Management company due to the ever-increasing requirement for their crane hire and elevated work platforms business to have traffic management in place for most of their jobs. Further, the values they had for their Smart Lift business aligned perfectly with traffic management.</p>
+        <p>The four aims of the company are:</p>
+        <ul>
+            <li>Safety - Lead by example</li>
+            <li>Reliability – Deliver on Promise</li>
+            <li>K.I.S.S. Theory – Do the Basics Brilliantly</li>
+            <li>Client is Number 1 – We Go Beyond the Call </li>
+        </ul>
+        <p>Approaching the end of their 2nd year of operation, STMSNZ is growing rapidly. Demand is driving the company to take on more staff, growing resources to be able to respond quickly to emergencies and unexpected work.</p>
+        <p>The recent launch of the STMSNZ ambassador ‘Roger the Road Cone’ is raising the awareness that safety is everything in this industry. He is dedicated to reminding everyone that workers are on the road every day, and we all have a part to play to be safe!</p>
+
+    </main>
+</body>
+
+</html>
---
+++ www/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>STMSNZ- Contact</title>
+    <link rel='shortcut icon' type='image/x-icon' href='images/favicon.ico' />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="nav-style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet" />
+
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="STMSNZ, STMS NZ, STMS, Traffic Managment">
+    <meta name="author" content="Hayden McAlister">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+    <?php include "navbar.php" ?>
+
+    <main>
+        <?php
+        $pagename = "Contact";
+        include "header.php"
+        ?>
+    </main>
+</body>
+
+</html>
