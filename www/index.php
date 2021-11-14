8302687107ce565548eb78e5ff95cf152bf24d92
Hayden McAlister <haydenmcalister49@gmail.com>
2021-11-14 22:28:36 +1300

    Update index to remove links to old slideshow and gallery js and css

--- www/index.php
+++ www/index.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="nav-style.css" />
-    <link rel="stylesheet" href="slideshow.css" />
-    <link rel="stylesheet" href="image-grid.css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet" />
 
     <meta charset="UTF-8">
@@ -16,8 +14,6 @@
     <meta name="keywords" content="STMSNZ, Traffic Management, Traffic Management Dunedin, STMS NZ, STMS">
     <meta name="author" content="Hayden McAlister">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="slideshow.js" defer></script>
 
 </head>
 
